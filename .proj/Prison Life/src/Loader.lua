d8e343a7e6afba951012a412dd811ef92aeba213
LLethul <119450941+LLethul@users.noreply.github.com>
2022-12-05 09:32:30 -0800

    Add basic shit

---
+++ .proj/Prison Life/src/Loader.lua
@@ -0,0 +1,31 @@
+local rootDir = "https://raw.githubusercontent.com/LLethul/LeHub/main/.proj/Prison%20Life/"
+local files = {
+    "lib/Blatant.lua",
+    "lib/UI.lua",
+    "src/Main.lua"
+}
+
+local betterisfile = function(file)
+	local suc, res = pcall(function() return readfile(file) end)
+	return suc and res ~= nil
+end
+
+function writeFile(name, src)
+    if isfolder("ixhub") then
+        if isfolder("ixhub/pl") then
+            writefile("ixhub/pl/"..name, src)
+        else
+            makefolder("ixhub/pl")
+        end
+    else
+        makefolder("ixhub")
+    end
+end
+
+for hash, file in pairs(files) do
+    local source = game:HttpGet(rootDir..file)
+    local waitbruh = string.split(file,"/")
+    waitbruh = waitbruh[#waitbruh]
+    print("Writing "..waitbruh.." to $WORKSPACE/ixhub/pl (...) ("..hash.." / "..#files..")")
+    writeFile(waitbruh, source)
+end
