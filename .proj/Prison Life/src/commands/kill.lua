7382d65e9e5cacb0d3baa25a885bdff9a5e6d3db
LLethul <119450941+LLethul@users.noreply.github.com>
2022-12-06 10:31:12 -0800

    fix

--- .proj/Prison Life/src/commands/kill.lua
+++ .proj/Prison Life/src/commands/kill.lua
@@ -7,7 +7,7 @@ return {
     run = function(Self, Args)
         for i,v in pairs(Args[1]) do
             getgenv().LHPLFuncs.Blatant.Kill(v)
-            if Self == getgenv().Funcs.GetLP() then
+            if Self == getgenv().LHPLFuncs.GetLP() then
                 getgenv().LHPLFuncs.UI.Notification("Killed "..v.Name)
             else
                 getgenv().LHPLFuncs.UI.Chat("Killed "..v.Name, Self)
