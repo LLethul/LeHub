da9a8ae6814b3d2b5846492d772f7f894f23f2b6
LLethul <119450941+LLethul@users.noreply.github.com>
2022-12-06 16:06:40 -0800

    fix da team thing

--- .proj/Prison Life/src/commands/team.lua
+++ .proj/Prison Life/src/commands/team.lua
@@ -9,11 +9,20 @@ return {
             Inmate = "Bright orange";
             Cop = "Really blue";
             Criminal = "Really red";
-            Neutral = "Medium stone grey"
+            Neutral = "Medium stone grey";
+            Prisoner = "Bright orange";
         }
 
         for i,v in pairs(teams) do
             if i:lower():find(Args[1]:lower()) then
+                if i=="Criminal" then
+                    local oldting = getgenv().LHPLFuncs.Blatant.GetPos()
+                    getgenv().LHPLFuncs.Blatant.TeamEvent(teams.Inmate)
+                    getgenv().LHPLFuncs.Blatant.Goto(getgenv().LHPLFuncs.Blatant.CFrames["Criminal Base"])
+                    wait(0.1)
+                    getgenv().LHPLFuncs.Blatant.Goto(oldting)
+                    return
+                end
                 getgenv().LHPLFuncs.Blatant.TeamEvent(v)
                 return
             end
